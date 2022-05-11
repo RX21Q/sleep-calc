62016e33e49cc177fc3940c0bbc4d2c9047878f3
pkhromchenko <43190693+pkhromchenko@users.noreply.github.com>
2022-05-10 20:55:33 -0400

    v0.6

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ImageBackground, Button, Alert, } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
   const [time, setTime] = useState(null);
@@ -10,23 +11,57 @@ export default function App() {
   }, []);
   const getCurrentTime = () => {
     let today = new Date();
-    let hours = (today.getHours() < 10 ? '0' : '') + today.getHours();
+    let hours = (today.getHours() < 10 ? '0' : '') + ((today.getHours() + 24) % 12 || 12);
     let minutes = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
-    let seconds = (today.getSeconds() < 10 ? '0' : '') + today.getSeconds();
-    return hours + ':' + minutes + ':' + seconds;
+    return hours + ':' + minutes;
   }
   let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const d = new Date();
   let monthName = months[d.getMonth()];
   var dateOfMonth = new Date().getDate();
-  var year = new Date().getFullYear(); 
+  var year = new Date().getFullYear();
+  var ampm = (d.getHours() >= 12) ? "PM" : "AM";
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground style={styles.background} source={require('./assets/background.png')}>
       </ImageBackground>
-      <Text style={styles.timeText}>{time}</Text>
+      <Text style={styles.timeText}>{time} {ampm}</Text>
       <Text style={styles.dateText}>{weekday}, {monthName} {dateOfMonth}, {year}</Text>
+      <View style={styles.break}/>
+      <View><Button onPress={showTimepicker} title="Show time picker!"/></View>
+      <Text>selected: {date.toLocaleString()}</Text>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={false}
+          onChange={onChange}
+        />
+      )}
       <Text style={styles.instOneTxt}>Desired Wake Up Time:</Text>
       <Button
         style={styles.buttonOne}
@@ -73,23 +108,30 @@ const styles = StyleSheet.create({
     color: 'black',
   },
   timeText: {
-    fontSize: 35,
+    fontSize: 32,
     color: 'white',
     textShadowColor: 'rgba(0, 0, 0, 0.75)',
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 10,
     position: 'absolute',
-    height: '92%',
+    top: '11%',
   },
   dateText: {
-    fontSize: 20,
+    fontSize: 22,
     color: 'white',
     textShadowColor: 'rgba(0, 0, 0, 0.75)',
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 10,
     position: 'absolute',
-    height: '78%',
-    
+    top: '6%',
+  },
+  break: {
+  borderBottomColor: 'black',
+  borderBottomWidth: 2,
+  alignSelf: 'auto',
+  width: '75%',
+  justifyContent: 'center',
+  bottom: '21%',
   },
   '*': {
     fontFamily: 'Roboto',
